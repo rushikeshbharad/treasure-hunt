8210a97004dcf6e25a749535f35a31528b842f12
rushikeshbharad <rushicbharad@gmail.com>
2019-08-19 16:05:02 +0530

    Added styling to home page and initiated treasure hunt blank screen

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 import config from './config';
 
 export default class App extends Component {
+  state = {
+    started: false,
+  };
+
+  beginClick = () => {
+    this.setState({ started: true });
+  };
+
   renderHome() {
     return (
       <div className="app-container">
         <div className="home">
           <div className="home-title">{config.homeTitle}</div>
           <div className="home-description">{config.homeDescription}</div>
+          <div className="home-button" onClick={this.beginClick}>Begin</div>
         </div>
       </div>
     );
   }
 
+  renderTreasureHunt() {
+    return null;
+  }
+
   render () {
+    const { started } = this.state;
+
     return (
       <Fragment>
-        {this.renderHome()}
+        {!started && this.renderHome()}
+        {started && this.renderTreasureHunt()}
       </Fragment>
     );
   }
