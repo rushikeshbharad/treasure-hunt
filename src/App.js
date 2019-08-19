5952eac3399d1547ae60e4fe42a8b5c395bb8b45
rushikeshbharad <rushicbharad@gmail.com>
2019-08-19 15:25:36 +0530

    Added home page initial styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import config from './config';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  renderHome() {
+    return (
+      <div className="app-container">
+        <div className="home">
+          <div className="home-title">{config.homeTitle}</div>
+          <div className="home-description">{config.homeDescription}</div>
+        </div>
+      </div>
+    );
+  }
+
+  render () {
+    return (
+      <Fragment>
+        {this.renderHome()}
+      </Fragment>
+    );
+  }
 }
-
-export default App;
