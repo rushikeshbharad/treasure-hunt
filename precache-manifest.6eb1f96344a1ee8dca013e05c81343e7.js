d24650fe50b9cddc973419e92b53e9a581aafa0e
rushikeshbharad <rushicbharad@gmail.com>
2019-08-22 21:54:49 +0530

    Updates

--- precache-manifest.6eb1f96344a1ee8dca013e05c81343e7.js
+++ precache-manifest.6eb1f96344a1ee8dca013e05c81343e7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "773273745e80b5dc2c51d0776e240b86",
+    "revision": "4b7f9c2bcad5cba558329feda0f0b1b0",
     "url": "/treasure-hunt/index.html"
   },
   {
-    "revision": "0c47b614252f1550448d",
+    "revision": "ffdcc6029c92d6e0a651",
     "url": "/treasure-hunt/static/css/main.4a69e3fd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/treasure-hunt/static/js/2.bf0efc10.chunk.js"
   },
   {
-    "revision": "0c47b614252f1550448d",
-    "url": "/treasure-hunt/static/js/main.f7b4f30e.chunk.js"
+    "revision": "ffdcc6029c92d6e0a651",
+    "url": "/treasure-hunt/static/js/main.6ff250a2.chunk.js"
   },
   {
     "revision": "250b2a2885d04cb663c9",
